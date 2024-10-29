7b7c0ca7a43c311c8cbd0665ec232c64da04afaf
Danny-Hash <dannyhash0x1@gmail.com>
2024-10-29 10:52:42 +0100

    fix: removed unnecesary padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg">
-        <div className="container px-4 mx-auto relative text-sm">
+        <div className="container mx-auto relative text-sm">
           <div className="flex justify-between items-center">
             <div className="flex items-center flex-shrink-0">
               <img className="mr-2" src={logo} alt="logo" />
