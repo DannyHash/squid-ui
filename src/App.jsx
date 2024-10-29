ad690dbfec08d944dbf93cf07b7c548c5c56afba
Danny-Hash <dannyhash0x1@gmail.com>
2024-10-29 03:50:29 +0100

    Imported Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="px-8 py-4 md:px-12 lg:px-20">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
